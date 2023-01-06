feadf7adb98ce8b308ceeee88ec9d3c642287854
lx544690189 <lixin544@163.com>
2023-01-06 10:34:06 +0800

    feat: language for en

--- .dumirc.ts
+++ .dumirc.ts
@@ -4,5 +4,10 @@ export default defineConfig({
   outputPath: 'docs-dist',
   themeConfig: {
     name: 'scorpio-adapter',
+    logo: '/logo.png',
   },
+  locales: [
+    { id: 'zh-CN', name: '中文' },
+    { id: 'en-US', name: 'English' },
+  ],
 });
